675d6a4e646bb3b24a7eb8600c2f99b456c57883
azamatnassir <azamatnsr@gmail.com>
2023-08-07 16:00:58 +0600

    commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.io;
 
 import java.io.File;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder str = new StringBuilder();
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            int c;
+            while ((c = inputStream.read()) != -1){
+                str.append((char) c);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(str);
+
+        Map<String, String> map = new HashMap<>();
+        String[] keyVals = str.toString().split("[\\r\\n]+");
+        for(String keyVal:keyVals)
+        {
+            String[] parts = keyVal.split(": ");
+            map.put(parts[0],parts[1]);
+        }
+
+        return new Profile(map.get("Name"), Integer.parseInt(map.get("Age")), map.get("Email"), Long.valueOf(map.get("Phone")));
     }
 }
